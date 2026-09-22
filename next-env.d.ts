d8a658f30435c918b7aa5eab3d2efc7db882b106
SaranAgam <agamsaran.as@gmail.com>
2026-09-22 23:50:08 +0530

    update from AGAM

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited ,, also the work
+// NOTE: This file should not be edited ,, also the work,, AGAM
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
